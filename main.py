3b17f41547c87d90f9e5e840b89d11b8b50ac889
yalong <36091667+yalong1999@users.noreply.github.com>
2023-10-07 16:50:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,6 @@ def get_birthday():
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
   return words.json()['data']['text']
 
 def get_random_color():
